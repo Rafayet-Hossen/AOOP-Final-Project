766ba2f9603d8e959ed0eba5cfbce32cc851a5dc
Rafayet Hossen <hossenmdrafayet@gmail.com>
2025-10-16 00:56:53 +0600

    fix some unwanted lines from my code also test the git

--- src/main/java/com/zcare/finalproject/AdminDashboardController.java
+++ src/main/java/com/zcare/finalproject/AdminDashboardController.java
@@ -2,7 +2,6 @@ package com.zcare.finalproject;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -13,8 +12,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.stage.Stage;
-
-
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
--- src/main/java/com/zcare/finalproject/DoctorsController.java
+++ src/main/java/com/zcare/finalproject/DoctorsController.java
@@ -8,13 +8,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.net.URL;
 import java.sql.ResultSet;
